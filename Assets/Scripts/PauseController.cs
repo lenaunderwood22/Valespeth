ccbc464816d4eb6987a6d9fef12795f829f87da8
EZWD <pzhang1@macalester.edu>
2020-05-06 02:28:32 +0800

    PauseBottomPart(1)

---
+++ Assets/Scripts/PauseController.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseController : MonoBehaviour
+{
+
+    public GameObject PausePanel;
+
+    void Start(){
+        PausePanel.SetActive(false);
+    }
+    public void PauseGame(){
+        PausePanel.SetActive(true);
+        Time.timeScale = 0f ;
+    }
+
+    public void ResumeGame(){
+        PausePanel.SetActive(false);
+        Time.timeScale = 1f ;
+    }
+
+    public void QuitGame(){
+        Application.Quit();
+    }
+
+    public void BackToMain(){
+        PausePanel.SetActive(false);
+        Time.timeScale = 1f ;
+        SceneManager.LoadScene("TitleScene");
+    }
+}
